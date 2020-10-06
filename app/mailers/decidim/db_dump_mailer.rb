3b5624cbbe355c4f1c50cd87a15dac5b11485f99
cedrtang <cedric@beyowi.com>
2020-10-06 11:45:55 -0700

    DbDump: mail send to all recipients in one

--- app/mailers/decidim/db_dump_mailer.rb
+++ app/mailers/decidim/db_dump_mailer.rb
@@ -6,12 +6,10 @@ module Decidim
     def send_dump(file_name, file_path) # rubocop:disable Metrics/ParameterLists
       # Need to get organization for smtp configuration
       @organization = Decidim::Organization.find(1)
-      return unless ENV["DB_DUMP_EMAILS"] && ENV["DB_DUMP_EMAILS"].split(";").size > 0
+      return unless ENV["DB_DUMP_EMAILS"]
 
       attachments[file_name] = File.read(file_path)
-      ENV["DB_DUMP_EMAILS"].split(";").each do |email|
-        mail(to: email, subject: "Decidim Database Dump #{Date.today}")
-      end
+      mail(to: ENV["DB_DUMP_EMAILS"], subject: "Decidim Database Dump #{Date.today}")
     end
   end
 end
